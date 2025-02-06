94585697dce2dc21debe80e7f7e590b50a23c5e3
njaalsb <bruheimnjal@gmail.com>
2025-02-06 11:06:27 +0100

    nye filer

---
+++ IIKT2105WebServer_Skeleton.py
@@ -0,0 +1,52 @@
+from socket import *
+# Additional imports for graceful shutdown.
+import signal
+import sys
+
+# Prepare sever socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+## TODO fill in start.
+# Set server port, bind socket, listen for connection.
+## TODO fill in end.
+
+
+# Handle Ctrl+C (SIGINT) to close the socket gracefully.
+def signal_handler(sig, frame):
+    print("\nShutting down the server")
+    serverSocket.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# Actual server logic
+while True:
+    # Establish the connection
+    print('Ready to serve')
+    connectionSocket, addr = ## TODO fill in.
+    try:
+        # Receive and decode request message from client.
+        request = ## TODO fill in.
+        # Extract file path from request and attempt to open it.
+        # Note: the path is relative to the location of the server program, i.e., a request for 
+        #       /index.html would attempt access the file index.html in the same folder as IIKT2105WebServer.py.
+        filename = request.split()[1]
+        f = open(filename[1:])
+        # Read the contents of the requested file into outputdata.
+        outputdata = ## TODO fill in.
+        ## TODO fill in start.
+        # Send the appropriate HTTP header line into the socket
+        # The basic format is "HTTP/1.1 CORRECTHTTPCODE\r\n\r\n", replacing CORRECTHTTPCODE with the actual code.
+        ## TODO fill in end.
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+    # Handle IOError when the file is not available.
+    except IOError:
+        ## TODO fill in start.
+        # Send the appropriate HTTP response message for file not found
+        ## TODO fill in end.
+    
+    ## TODO fill in start.
+    #Close client's connection socket
+    ## TODO fill in end.
